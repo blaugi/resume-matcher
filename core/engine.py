dfeadede8afc335880e3a722b7a6a7b7059f9e84
Blaugi <matheusmgirardi@gmail.com>
2026-02-26 15:13:32 -0300

    feat: flesh out engine, add uuid to chunks

--- core/engine.py
+++ core/engine.py
@@ -1,14 +1,11 @@
-from core.models import TextChunk, LoadedDocument, ChunkMatch
+import numpy as np
 
 # from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-from langchain_docling.loader import DoclingLoader
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-from langchain.chat_models import init_chat_model
-from dotenv import load_dotenv
+
+from core.models import ChunkMatch, LoadedDocument, TextChunk
 
 
 class ResumeEngine:
@@ -25,23 +22,29 @@ class ResumeEngine:
 
         self.current_resume: LoadedDocument | None = None
         self.current_job: LoadedDocument | None = None
+        self.match_list: list[ChunkMatch]| None = None
+
     
     def load_resume(self, path: str):
         self.current_resume = LoadedDocument.create_from_path(
             path, self.semantic_splitter, self.embeddings
         )
-    def load_job(self, path: str):
-        self.current_job= LoadedDocument.create_from_path(
-            path, self.semantic_splitter_low, self.embeddings
-        )
+    def load_job(self, input_str: str, path:bool = False):
+        if path:
+            self.current_job= LoadedDocument.create_from_path(
+                input_str, self.semantic_splitter_low, self.embeddings
+            )
+        else:
+            self.current_job = LoadedDocument.create_from_text(
+                input_str, self.semantic_splitter_low, self.embeddings
+            )
 
-    def update_resume_chunk(self, chunk_index: int, new_text: str):
+    def update_resume_chunk(self, chunk_id: str, new_text: str):
         """Update text and re-vectorize (Write)"""
-        if self.current_resume:
-            chunk = self.current_resume.chunks[chunk_index]
-            
-            chunk.update_text(new_text, self.embeddings) 
-            self.current_resume._vectorize_self(self.embeddings)
+        match = self.get_match_from_uuid(chunk_id)
+        if self.current_resume and match:
+                match.resume_chunk.update_text(new_text, self.embeddings) 
+                self.current_resume._vectorize_self(self.embeddings)
 
     def get_resume_chunks(self) -> list[TextChunk]:
         return self.current_resume.chunks if self.current_resume else []
@@ -85,3 +88,24 @@ class ResumeEngine:
         self.match_list = matches
         return matches
      
+    def get_match_from_uuid(self, uuid:str) -> ChunkMatch | None:
+        if not self.match_list:
+            return None
+        
+        for match in self.match_list:
+            if match.chunk_id == uuid:
+                found_match = match
+                break
+        else:
+            found_match= None 
+        return found_match 
+
+    def get_match_texts(self, uuid:str) -> tuple[str,str]:
+        """ Returns job_text, resume_text as strings"""
+        chunk_match = self.get_match_from_uuid(uuid)
+        if chunk_match:
+            return chunk_match.get_job_text(), chunk_match.get_resume_text()
+        else:
+            return "Error", "Error"
+
+        
--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
-from langchain_core.documents import Document
-from dataclasses import dataclass
-from langchain_docling.loader import DoclingLoader
+import uuid
+from dataclasses import dataclass, field
+
 from langchain_community.document_loaders import TextLoader
+from langchain_core.documents import Document
+from langchain_docling.loader import DoclingLoader
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
@@ -26,10 +28,12 @@ class LoadedDocument:
         cls, path: str, chunker: SemanticChunker, embedding_model: HuggingFaceEmbeddings
     ):
         match path.split(".")[1]:
-            case ".pdf":
+            case "pdf":
                 document = DoclingLoader(path).load()
-            case ".txt" | ".typ":
+            case "txt" | "typ":
                 document = TextLoader(path).load()
+            case _:
+                raise Exception("Unsupported file type.")
 
         if document_text := document[0].page_content:
             full_vector = embedding_model.embed_query(document_text)
@@ -43,6 +47,29 @@ class LoadedDocument:
             chunks.append(TextChunk(chunk, chunk_embeddings[i]))
 
         return cls(chunks, full_vector)
+
+    @classmethod
+    def create_from_text(
+        cls, text: str, chunker: SemanticChunker, embedding_model: HuggingFaceEmbeddings
+    ):
+        document = Document(
+            page_content=text,
+            metadata={"source": "in-memory string", "date": "2026-02-26"}
+        )
+
+        if document_text := document.page_content:
+            full_vector = embedding_model.embed_query(document_text)
+
+        chunked_document = chunker.split_documents([document])
+        chunked_document_texts = [doc.page_content for doc in chunked_document]
+        chunk_embeddings = embedding_model.embed_documents(chunked_document_texts)
+
+        chunks = []
+        for i, chunk in enumerate(chunked_document):
+            chunks.append(TextChunk(chunk, chunk_embeddings[i]))
+
+        return cls(chunks, full_vector)
+    
 
     # This is assuming the order of the chunks wont change
     def get_full_text(self) -> str:
@@ -60,6 +87,7 @@ class ChunkMatch:
     job_chunk: TextChunk
     similarity: float
     status: str
+    chunk_id: str = field(default_factory=lambda: str(uuid.uuid4()))
 
     def get_job_text(self) -> str:
         return self.job_chunk.document.page_content
